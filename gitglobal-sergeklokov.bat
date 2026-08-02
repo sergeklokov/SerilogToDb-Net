aaf8f83cc7d65db18062023af47c4e38bf54c0b3
sergeklokov <serge.klokov@gmail.com>
2026-08-01 18:11:34 -0700

    init

---
+++ gitglobal-sergeklokov.bat
@@ -0,0 +1,7 @@
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global user.name "sergeklokov"
+git config --global user.email "serge.klokov@gmail.com"
+git config user.name
+git config user.email
+git config --show-origin --list
